79853d8f22e4a33c5e9221beddf2fe0fff4c2409
Ali Hassan <engineralihassan@gmail.com>
2024-08-05 07:36:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs= require('fs');
+let hello = "Hello world its an first node script";
+let modules = "Hello this is modules intro lecture";
+
+console.log(hello);
+console.log(modules);
+
+
+

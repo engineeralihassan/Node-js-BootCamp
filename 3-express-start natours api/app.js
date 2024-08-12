6c770067059090c9e954a9a2b8b223f8b5a6868e
AliHassan <engineralihassan@gmail.com>
2024-08-13 00:07:24 +0500

    db connection successfull

--- 3-express-start natours api/app.js
+++ 3-express-start natours api/app.js
@@ -19,18 +19,7 @@ app.use((req,res,next)=>{
 });
 
 app.use(express.static(`${__dirname}/public`))
-
-
-
-
-
-
   app.use('/api/v1/tours',tourRouter);
   app.use('/api/v1/users',userRouter);
-
-
-
-
-
 module.exports=app;
 

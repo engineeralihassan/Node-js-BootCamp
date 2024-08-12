7726b9aeed3d73515629415f0a96320afb90cd7d
Ali Hassan <engineralihassan@gmail.com>
2024-08-11 23:59:21 -0400

    better way of creating routes in node

--- 3-express-start natours api/app.js
+++ 3-express-start natours api/app.js
@@ -19,62 +19,168 @@ let tours = JSON.parse(fileContent);
 
 // Make some URLS according to the REST structure
 
-app.get('/api/v1/tours', (req, res) => {
-  console.log('re');
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
+// app.get('/api/v1/tours', (req, res) => {
+//   console.log('re');
+//   res.status(200).json({
+//     status: 'success',
+//     results: tours.length,
+//     data: {
+//       tours,
+//     },
+//   });
+// });
 
 // POST REUESTS
 
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-  let newId = tours[tours.length - 1].id + 1;
-  let newTour = Object.assign({ id: newId }, req.body);
-  fs.writeFile(
-    './dev-data/data/tours-simple.json',
-    JSON.stringify(tours),
-    (err, data) => {
-      if (err) {
-        return;
-      }
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-  //  res.send('Done');
-});
+// app.post('/api/v1/tours', (req, res) => {
+//   // console.log(req.body);
+//   let newId = tours[tours.length - 1].id + 1;
+//   let newTour = Object.assign({ id: newId }, req.body);
+//   fs.writeFile(
+//     './dev-data/data/tours-simple.json',
+//     JSON.stringify(tours),
+//     (err, data) => {
+//       if (err) {
+//         return;
+//       }
+//       res.status(200).json({
+//         status: 'success',
+//         data: {
+//           tour: newTour,
+//         },
+//       });
+//     }
+//   );
+//   //  res.send('Done');
+// });
 
 // Read PARAMS FROM URL
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+// app.get('/api/v1/tours/:id', (req, res) => {
+//   console.log(req.params);
+//   const id = req.params.id * 1;
+//   const tour = tours.find((el) => el.id === id);
 
-  if (!tour) {
-    res.status(404).json({
-      status: 'success',
-      message: 'No Record found againt this Id',
-    });
-  } else {
+//   if (!tour) {
+//     res.status(404).json({
+//       status: 'success',
+//       message: 'No Record found againt this Id',
+//     });
+//   } else {
+//     res.status(200).json({
+//       status: 'success',
+//       data: {
+//         tour,
+//       },
+//     });
+//   }
+// });
+
+// PUT OR PATH | UPDATE THE DATA
+
+// app.patch('/api/v1/tours/:id', (req, res) => {
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       record: '< updated record is here>',
+//     },
+//   });
+// });
+
+// DELETE A RECORD
+
+// app.delete('/api/v1/tours/:id', (req, res) => {
+//     res.status(200).json({
+//       status: 'success',
+//       data: {
+//         record: null,
+//       },
+//     });
+//   });
+  
+  // BETTER VERSIONINGS OF THE ROUTES
+
+  const getAllTours= (req, res) => {
+    console.log('re');
     res.status(200).json({
       status: 'success',
+      results: tours.length,
       data: {
-        tour,
+        tours,
       },
     });
   }
-});
+  const createTour= (req, res) => {
+    let newId = tours[tours.length - 1].id + 1;
+    let newTour = Object.assign({ id: newId }, req.body);
+    fs.writeFile(
+      './dev-data/data/tours-simple.json',
+      JSON.stringify(tours),
+      (err, data) => {
+        if (err) {
+          return;
+        }
+        res.status(200).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        });
+      }
+    );
+    //  res.send('Done');
+  }
+const getTour=  (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+  
+    if (!tour) {
+      res.status(404).json({
+        status: 'success',
+        message: 'No Record found againt this Id',
+      });
+    } else {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tour,
+        },
+      });
+    }
+  }
+
+  const deleteTour= (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        record: null,
+      },
+    });
+  }
+
+  const updateTour= (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        record: '< updated record is here>',
+      },
+    });
+  }
+
+
+
+//   app.get('/api/v1/tours',getAllTours);
+//   app.get('/api/v1/tours/:id',getTour);
+//   app.patch('/api/v1/tours/:id',updateTour);
+//   app.delete('/api/v1/tours/:id',deleteTour);
+
+
+// LET iNRTODUCE THE ROUTES IN NODE JS 
+
+app.route('api/v1/tours').get(getAllTours).post(createTour);
+app.route('api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
 
 app.listen(port, () => {
   console.log('Hello this is the app running on port');

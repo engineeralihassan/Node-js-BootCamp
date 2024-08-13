3a238b6b05d3e6341d500b517c69924d450d4729
Ali Hassan <engineralihassan@gmail.com>
2024-08-12 23:43:53 -0400

    Complete the tour model for the section 5

--- 3-express-start natours api/modals/tourModal.js
+++ 3-express-start natours api/modals/tourModal.js
@@ -4,17 +4,61 @@ const tourSchema= new mongoose.Schema({
     name:{
         type:String,
         required:[true,'A tour must have a name'],
-        unique:true
+        unique:true,
+        trim:true
+    },
+    duration:{
+        type:Number,
+        required:[true,'A Tour must have a duration']
+    },
+    maxGroupSize:{
+        type:Number,
+        required:[true,'A Tour must have a Group size']
+    },
+    difficulty:{
+        type:String,
+        required:[true,'A Tour must have a difficulty']
+    },
+    ratingsAverage:{
+        type:Number,
+      default:4.5
+    },
+    ratingsQuantity:{
+        type:Number,
+      default:0
     },
     rating:{
         type:Number,
       default:4.5
     },
+
     price:{
         type:Number,
         required:[true,'A tour must have a price'],
         
     },
+
+    priceDiscount:{
+        type:Number,
+    },
+    summry:{
+        type:String,
+        trim:true
+    },
+    description:{
+        type:String,
+     trim:true
+    },
+    imageCover:{
+        type:String,
+        required:[true,'A tour must have an cover image']
+    },
+    images:[String],
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    strtDates:[Date]
 
 
 })

3f2950f5bb25a004e3fa21cc8e3d5647e0179a68
Ali Hassan <engineralihassan@gmail.com>
2024-08-15 00:25:27 -0400

    Handle errors is na separte class

--- 3-express-start natours api/app.js
+++ 3-express-start natours api/app.js
@@ -4,6 +4,8 @@ const morgan= require('morgan');
 const fs = require('fs');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError =require("./utils/appError");
+const  globalErrorHandler= require("./controllers/errorController")
 const app = express();
 
 //  Middle-wares 
@@ -21,5 +23,18 @@ app.use((req,res,next)=>{
 app.use(express.static(`${__dirname}/public`))
   app.use('/api/v1/tours',tourRouter);
   app.use('/api/v1/users',userRouter);
+
+// UnMatched Route 
+// Catch-all route handler for undefined routes
+app.all('*', (req, res, next) => {
+  // const err = new Error(`The Endpoint ${req.originalUrl} is not defined`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`The Endpoint ${req.originalUrl} is not defined`,404));
+});
+
+// Error handling middleware
+app.use(globalErrorHandler);
+
 module.exports=app;
 

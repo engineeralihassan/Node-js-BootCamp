2e4a5933f29262128d43bbd71ca80fb680f812e3
Ali Hassan <engineralihassan@gmail.com>
2024-08-15 02:02:57 -0400

    Error Handling in Express is done

--- 3-express-start natours api/controllers/errorController.js
+++ 3-express-start natours api/controllers/errorController.js
@@ -1,8 +1,41 @@
+const { stack } = require("../app");
+
+const sendErrorDev=(err,res)=>{
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        stack:err.stack,
+        error:err
+      });
+}
+const sendErrorProd=(err,res)=>{
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+          });
+    }else{
+        res.status(err.statusCode).json({
+            status: 'fail',
+            message: 'Something went very wrong',
+          }); 
+    }
+ 
+}
+
+
 module.exports=(err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
+    // res.status(err.statusCode).json({
+    //   status: err.status,
+    //   message: err.message
+    // });
+
+    if(process.env.NODE_ENV === 'development'){
+        sendErrorDev(err,res);
+    }
+    else{
+        sendErrorProd(err,res)
+    }
   }

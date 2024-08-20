1182ac11ef617046a7e347130303b3035e6ab6c4
AliHassan <engineralihassan@gmail.com>
2024-08-21 03:08:03 +0500

    refactor code with jonas code

--- 3-express-start natours api/controllers/authenticationController.js
+++ 3-express-start natours api/controllers/authenticationController.js
@@ -13,6 +13,30 @@ let createToken=(userId)=>{
 
 }
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  
+    res.cookie('jwt', token, cookieOptions);
+  
+    // Remove password from output
+    user.password = undefined;
+  
+    res.status(statusCode).json({
+      status: 'success',
+      token,
+      data: {
+        user
+      }
+    });
+  };
+
 exports.signUp= catchAsync(  async(req,res,next)=>{
     let user = await User.create(req.body);
     let token= createToken(user._id);
@@ -101,37 +125,6 @@ exports.protect = catchAsync(async (req, res, next) => {
     }
   }
 
-<<<<<<< HEAD
-});
-
-
-exports.resetPassword = catchAsync(async (req, res, next) => {
-    // 1) Get user based on the token
-    const hashedToken = crypto
-      .createHash('sha256')
-      .update(req.params.token)
-      .digest('hex');
-  
-    const user = await User.findOne({
-      passwordResetToken: hashedToken,
-      passwordResetExpires: { $gt: Date.now() }
-    });
-  
-    // 2) If token has not expired, and there is user, set the new password
-    if (!user) {
-      return next(new AppError('Token is invalid or has expired', 400));
-    }
-    user.password = req.body.password;
-    user.passwordConfirm = req.body.passwordConfirm;
-    user.passwordResetToken = undefined;
-    user.passwordResetExpires = undefined;
-    await user.save();
-  
-    // 3) Update changedPasswordAt property for the user
-    // 4) Log the user in, send JWT
-    createSendToken(user, 200, res);
-  });
-=======
   exports.forgotPassword = catchAsync(async (req, res, next) => {
     console.log("We are ressting passwords");
     // 1) Get user based on POSTed email
@@ -174,5 +167,33 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
       );
     }
   });
-  exports.resetPassword=catchAsync(async (req, res, next) => {});
->>>>>>> 6b7afd62d3abbbb03619f93e955e24577f2de07c
+
+
+
+  exports.resetPassword = catchAsync(async (req, res, next) => {
+    // 1) Get user based on the token
+    const hashedToken = crypto
+      .createHash('sha256')
+      .update(req.params.token)
+      .digest('hex');
+  
+    const user = await User.findOne({
+      passwordResetToken: hashedToken,
+      passwordResetExpires: { $gt: Date.now() }
+    });
+  
+    // 2) If token has not expired, and there is user, set the new password
+    if (!user) {
+      return next(new AppError('Token is invalid or has expired', 400));
+    }
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+  
+    // 3) Update changedPasswordAt property for the user
+    // 4) Log the user in, send JWT
+    createSendToken(user, 200, res);
+
+  });

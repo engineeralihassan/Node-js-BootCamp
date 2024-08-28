0c4c8c313f3a30190c5573a9a6e9423675c40ea2
AliHassan <engineralihassan@gmail.com>
2024-08-28 14:48:22 +0500

    populating and referencing data in mongoose

--- 3-express-start natours api/modals/tourModal.js
+++ 3-express-start natours api/modals/tourModal.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -103,7 +103,13 @@ const tourSchema = new mongoose.Schema(
         day:Number
       }
     ],
-    guides:Array
+    guides: [
+      {
+        type:mongoose.Schema.ObjectId,
+        ref:'User'
+      } 
+      
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -121,11 +127,11 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save',async function(next) {
-  let guidesPromises = this.guides.map(async(id)=> await User.findById(id));
-  this.guides= await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save',async function(next) {
+//   let guidesPromises = this.guides.map(async(id)=> await User.findById(id));
+//   this.guides= await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
@@ -148,6 +154,14 @@ tourSchema.pre(/^find/, function(next) {
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select:"-__v -passwordResetExpires -role"
+  });
+
   next();
 });
 

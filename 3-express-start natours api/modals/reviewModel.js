b18822889f47407ebeb0ef1f560aa88e850b67b4
AliHassan <engineralihassan@gmail.com>
2024-08-28 15:51:08 +0500

    populating the user and tour in review

--- 3-express-start natours api/modals/reviewModel.js
+++ 3-express-start natours api/modals/reviewModel.js
@@ -1,6 +1,6 @@
 // review / rating / createdAt / ref to tour / ref to user
 const mongoose = require('mongoose');
-const Tour = require('./tourModel');
+const Tour = require('./tourModal');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -36,21 +36,21 @@ const reviewSchema = new mongoose.Schema(
 
 // reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
-// reviewSchema.pre(/^find/, function(next) {
-  // this.populate({
-  //   path: 'tour',
-  //   select: 'name'
-  // }).populate({
-  //   path: 'user',
-  //   select: 'name photo'
-  // });
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
 
 //   this.populate({
 //     path: 'user',
 //     select: 'name photo'
 //   });
-//   next();
-// });
+  next();
+});
 
 // reviewSchema.statics.calcAverageRatings = async function(tourId) {
 //   const stats = await this.aggregate([

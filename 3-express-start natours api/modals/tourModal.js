c7c0b55af3c787cc41e62a09418bdacfb02540f4
AliHassan <engineralihassan@gmail.com>
2024-08-28 12:46:04 +0500

    Embedding documents users in tours

--- 3-express-start natours api/modals/tourModal.js
+++ 3-express-start natours api/modals/tourModal.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -77,7 +78,32 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation:{
+      type:{
+        type:String,
+        default:'Point',
+        enum:['Point']
+      },
+      coordinates:[Number],
+      address:String,
+      description:String,
+
+
+    },
+    locations:[
+      {
+        type:{
+          type:String,
+          default:'Point',
+          enum:['Point']
+        },
+        coordinates:[Number],
+        address:String,
+        day:Number
+      }
+    ],
+    guides:Array
   },
   {
     toJSON: { virtuals: true },
@@ -92,6 +118,12 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save',async function(next) {
+  let guidesPromises = this.guides.map(async(id)=> await User.findById(id));
+  this.guides= await Promise.all(guidesPromises);
   next();
 });
 

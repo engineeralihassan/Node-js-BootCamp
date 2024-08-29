3dee189b569122e4bc469e918eccad9bd7fffc24
AliHassan <engineralihassan@gmail.com>
2024-08-29 08:38:20 +0500

    review virtual populating in node

--- 3-express-start natours api/modals/tourModal.js
+++ 3-express-start natours api/modals/tourModal.js
@@ -120,6 +120,12 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+// Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {

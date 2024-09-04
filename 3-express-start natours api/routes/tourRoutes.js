22c9d6b5a06ca593b4ec50eeeeae4b328e583acb
AliHassan <engineralihassan@gmail.com>
2024-09-04 22:35:55 +0500

    get routes geo location queries

--- 3-express-start natours api/routes/tourRoutes.js
+++ 3-express-start natours api/routes/tourRoutes.js
@@ -27,13 +27,13 @@ router
     tourController.getMonthlyPlan
   );
 
-// router
-//   .route('/tours-within/:distance/center/:latlng/unit/:unit')
-//   .get(tourController.getToursWithin);
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
 // /tours-within?distance=233&center=-40,45&unit=mi
 // /tours-within/233/center/-40,45/unit/mi
 
-// router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 
 router
   .route('/')

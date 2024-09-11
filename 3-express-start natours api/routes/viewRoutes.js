30f203ece012b08d3b753779729060e4797f109b
AliHassan <engineralihassan@gmail.com>
2024-09-11 20:55:25 +0500

    vits upload file and email templates are done

--- 3-express-start natours api/routes/viewRoutes.js
+++ 3-express-start natours api/routes/viewRoutes.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
-router.use(authController.isLoggedIn);
-router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+
+router.get(
+  '/my-tours',
+  bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
 
 router.post(
   '/submit-user-data',
   authController.protect,
   viewsController.updateUserData
 );
-// 2JAXPVHG7XUU3J8AKEE2QVQX
+
 module.exports = router;
